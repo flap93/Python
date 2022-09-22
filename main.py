da05ad3bc61cd56eb410ac7dddc6ade308820f4e
FELIX A PALACIOS <71362706+flap93@users.noreply.github.com>
2022-09-22 22:05:10 +0000

    add nestes functions

--- main.py
+++ main.py
@@ -6,13 +6,15 @@
 # print("The function is declared like this:")
 # print("print('what to print')")
 
-
 # print("Day 1 - String Manipulation")
 # print('String Concatenation is done with the "+" sign.')
 # print('e.g. print("Hello " + "world")')
 # print("New lines can be created with a backslash and n.")
 
-
 # ---------------------------------------------------------
-
+# NESTED FUNCTION AND PRINT
 print("Hello " + input("what is your name?"))
+
+# NESTED FUNCTIONS
+
+print(len(input("what is your name?")))
