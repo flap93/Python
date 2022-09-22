826e95f50830b5c0c926e13978f1de8352c5cda6
FELIX A PALACIOS <71362706+flap93@users.noreply.github.com>
2022-09-22 21:28:10 +0000

    add input and print

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 # Write your code below this line 👇
 
-print("Hello" + " Felix")
+# print("Hello" + " Felix")
 
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print('what to print')")
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
 
 
-print("Day 1 - String Manipulation")
-print('String Concatenation is done with the "+" sign.')
-print('e.g. print("Hello " + "world")')
-print("New lines can be created with a backslash and n.")
+# print("Day 1 - String Manipulation")
+# print('String Concatenation is done with the "+" sign.')
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+
+# ---------------------------------------------------------
+
+print("Hello " + input("what is your name?"))
