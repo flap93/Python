17f23ace9f660fb4ed1cab8784a10103fcb9ab83
FELIX A PALACIOS <71362706+flap93@users.noreply.github.com>
2022-09-22 08:47:19 +0000

    add more printing

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 # Write your code below this line 👇
 
-print("Hello world")
+print("Hello" + " Felix")
 
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
+
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
