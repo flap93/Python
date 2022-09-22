5bc15c05a442b3c5b0e502979ad3dec5589516cc
FELIX A PALACIOS <71362706+flap93@users.noreply.github.com>
2022-09-22 08:05:06 +0000

    add print

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # Write your code below this line 👇
 
-print("Hello world");
+print("Hello world")
 
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
