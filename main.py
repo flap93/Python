1d9eaab6f3d48648154ace08c3e4824b8cae73e8
FELIX A PALACIOS <71362706+flap93@users.noreply.github.com>
2022-09-23 04:31:06 +0000

    add variables and input

--- main.py
+++ main.py
@@ -13,8 +13,39 @@
 
 # ---------------------------------------------------------
 # NESTED FUNCTION AND PRINT
-print("Hello " + input("what is your name?"))
+# print("Hello " + input("what is your name?"))
 
-# NESTED FUNCTIONS
+# # NESTED FUNCTIONS
 
-print(len(input("what is your name?")))
+# print(len(input("what is your name?")))
+
+#------------------------------------------------------------
+
+
+
+name = input("What is your name?");
+
+length = len(name);
+
+print(length);
+
+#----------------------------------
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+c = a 
+a = b
+b = c
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
