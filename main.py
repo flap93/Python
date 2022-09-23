90eee42395cbb0ad0ad3812eb2fbd10ad6f09933
FELIX A PALACIOS <71362706+flap93@users.noreply.github.com>
2022-09-23 21:11:10 +0000

    add exercise tip calculator

--- main.py
+++ main.py
@@ -137,7 +137,7 @@ age = input("What is your current age?\n")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-
+total_days
 days_of_ninty = 90 * 365
 age_on_days = int(age) * 365
 total_days = days_of_ninty - age_on_days
@@ -153,3 +153,29 @@ total_months = months_of_ninty - age_on_months
 print(
     f"You have {total_days} days. {total_weeks} weeks, and {total_months} months left"
 )
+
+#-----------------------------------------------------------
+#------------------------TIP CALCULATOR-----------------
+
+#If the bill was $150.00, split between 5 people, with 12% tip.
+
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the result to 2 decimal places = 33.60
+
+#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+
+#Write your code below this line 👇
+
+print("Welcome to the tip calculator!")
+bill = float(input("What is the total bill?\n $"))
+tip = input("How much tip would you like to give ? 10, 12, or 15?")
+people = int(input("How many people to split the bill?\n"))
+tip_as_percent = tip / 100
+
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
+final_amount = "{:.f}".format(bill_per_person)
+
+print(f"Each person should pay ${final_amount}")
